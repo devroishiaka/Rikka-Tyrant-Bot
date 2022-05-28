d1e12c100145b221a596c9da01e3300721cba6d0
Yash <yashprakash2005@gmail.com>
2022-05-28 23:00:32 +0530

    hmm v2

--- Yumeko/ex_plugins/dbfunctions.py
+++ Yumeko/ex_plugins/dbfunctions.py
@@ -2,7 +2,7 @@ import codecs
 import pickle
 from typing import Dict, List, Union
 
-from Shikimori.mongo import db
+from Yumeko.mongo import db
 
 # SOME THINGS ARE FUCKED UP HERE, LIKE TOGGLEABLES HAVE THEIR OWN COLLECTION
 # (SHOULD FIX IT WITH SOMETHING LIKE TOGGLEDB), MOST OF THE CODE IS BAD AF
