f9a11fbba3eefd12ae0ea1d7ea8528a4283f89f0
Yash <yashprakash2005@gmail.com>
2022-06-08 01:27:00 +0530

    Update alive.py

--- Yumeko/modules/alive.py
+++ Yumeko/modules/alive.py
@@ -1,29 +1,25 @@
-import re
-import os
-
-from telethon import events, Button
+from telethon import Button
+from telegram import __version__ as telever
 from telethon import __version__ as tlhver
 from pyrogram import __version__ as pyrover
-from Yumeko.events import register as MEMEK
+from Yumeko.events import register
 from Yumeko import telethn as tbot
 
 PHOTO = "https://telegra.ph/file/aade65f7e2cf96064e12d.mp4"
 
-@MEMEK(pattern=("/alive"))
+@register(pattern=("/alive"))
 async def awake(event):
-  tai = event.sender.first_name
-  RIKKA = "**Moshi Moshi I'm Rikka!** \n\n"
-  RIKKA += "×**I'm Working Properly** \n\n"
-  RIKKA += "×**My Owner : [Sneha Jha](https://t.me/Sneha_UwU_OwO)** \n\n"
-  RIKKA += f"×**Telethon Version : {tlhver}** \n\n"
-  RIKKA += f"×**Pyrogram Version : {pyrover}** \n\n"
-  RIKKA += "**Thanks For Adding Me Here ❤️**"
+  TEXT = "**Moshi Moshi I'm Rikka!** \n\n"
+  TEXT += "×**I'm Working Properly** \n\n"
+  TEXT += "×**My Owner : [Sneha Jha](https://t.me/Sneha_UwU_OwO)** \n\n"
+  TEXT += f"×**Telethon Version : {tlhver}** \n\n"
+  TEXT += f"×**Pyrogram Version : {pyrover}** \n\n"
+  TEXT += "**Thanks For Adding Me Here ❤️**"
   BUTTON = [[Button.url("ʜᴇʟᴘ", "https://t.me/Rikka_Tyrant_bot?start=help"), Button.url("Support", "https://t.me/tyranteyeeee")]]
-  await tbot.send_file(event.chat_id, PHOTO, caption=RIKKA,  buttons=BUTTON)
+  await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
 
-@MEMEK(pattern=("/reload"))
+@register(pattern=("/reload"))
 async def reload(event):
-  tai = event.sender.first_name
-  RIKKA = "✅ **bot restarted successfully**\n\n• Admin list has been **updated**"
+  TEXT = "✅ **bot restarted successfully**\n\n• Admin list has been **updated**"
   BUTTON = [[Button.url("📡 ᴜᴘᴅᴀᴛᴇs", "https://t.me/tyranteyeeee")]]
-  await tbot.send_file(event.chat_id, PHOTO, caption=RIKKA,  buttons=BUTTON)
+  await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
