92706303cffb216ce50367588bc2d007afd7cc0f
Yash <yashprakash2005@gmail.com>
2022-06-08 01:57:55 +0530

    Revert "hmm"
    This reverts commit 2643828c11c6d46e504b98b2ce92155f59078610.

--- Yumeko/modules/shippering.py
+++ Yumeko/modules/shippering.py
@@ -1,33 +1,11 @@
 from Yumeko import pbot as app
 from Yumeko.utils.errors import capture_err
+from Yumeko.utils.dbfunc import get_couple, save_couple
 from pyrogram import filters
 import random
 from datetime import datetime
-from mongo import db
-
-coupledb = db.couple
-
-async def _get_lovers(chat_id: int):
-    lovers = coupledb.find_one({"chat_id": chat_id})
-    if not lovers:
-        return {}
-    return lovers["couple"]
-
-async def get_couple(chat_id: int, date: str):
-    lovers = await _get_lovers(chat_id)
-    if date in lovers:
-        return lovers[date]
-    return False
 
 
-async def save_couple(chat_id: int, date: str, couple: dict):
-    lovers = await _get_lovers(chat_id)
-    lovers[date] = couple
-    coupledb.update_one(
-        {"chat_id": chat_id},
-        {"$set": {"couple": lovers}},
-        upsert=True,
-    )
 # Date and time
 def dt():
     now = datetime.now()
