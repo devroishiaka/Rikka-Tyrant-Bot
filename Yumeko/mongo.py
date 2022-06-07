2643828c11c6d46e504b98b2ce92155f59078610
Yash <yashprakash2005@gmail.com>
2022-06-08 01:51:44 +0530

    hmm

--- Yumeko/mongo.py
+++ Yumeko/mongo.py
@@ -6,6 +6,7 @@ from Yumeko import MONGO_DB_URI
 from pymongo import MongoClient
 from pymongo.errors import ServerSelectionTimeoutError
 from Yumeko.conf import get_int_key, get_str_key
+from Yumeko.utils.logger import log
 
 
 MONGO_PORT = get_int_key("27017")
