e2a18172bcf45fa37954eb36e979cfdce42cbe03
devroishiaka <108065240+devroishiaka@users.noreply.github.com>
2022-07-03 18:52:31 +0530

    named modules

---
+++ Yumeko/modules/nightmode.py
@@ -0,0 +1,9 @@
+__mod_name__ = "Nɪɢʜᴛ-Mᴏᴅᴇ"
+
+__help__ = """
+Night Mode chats get Automatically closed at 12 am(IST)
+and Automatically openned at 6 am(IST) To Prevent Night Spams.
+
+❍ `/nightmode ` - Shows the current settings of nightmode
+❍ `/nightmode ` <on/off> will toggle our nightmode feature
+"""
