07045c23eeca2fb4c54c127c3ad6b9f6c71c6052
devroishiaka <108065240+devroishiaka@users.noreply.github.com>
2022-07-07 17:49:18 +0530

    Update _pyrogithub.py

--- Yumeko/modules/_pyrogithub.py
+++ Yumeko/modules/_pyrogithub.py
@@ -5,8 +5,23 @@ from Yumeko import pbot
 from Yumeko.pyrogramee.errors import capture_err
 
 
-__mod_name__ = "ɢɪᴛʜᴜʙ"
+__mod_name__ = "Gɪᴛʜᴜʙ"
 
+__help__ = """
+*Github module. This module will fetch github releases*\n
+*Available commands:*
+ • /github <user> : will fetch the most recent GitHub user
+ • /git <user>/<repo>: will fetch the most recent release from that repo.
+ • /git <user>/<repo> <number>: will fetch releases in past.
+ • /fetch <reponame> or &reponame`: same as /git , but you can use a saved repo shortcut
+ • /listrepo: lists all repo shortcuts in chat
+ • /gitver: returns the current API version
+ • /changelog <reponame>: gets the changelog of a saved repo shortcut
+ 
+*Admin only:*
+ • /saverepo <name> <user>/<repo> <number (optional)>: saves a repo value as shortcut
+ • /delrepo <name>: deletes a repo shortcut
+"""
 
 @pbot.on_message(filters.command('github'))
 @capture_err
