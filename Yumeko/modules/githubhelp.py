83e54258f9466ce712515208f94c14f716838ae0
devroishiaka <108065240+devroishiaka@users.noreply.github.com>
2022-07-08 16:03:38 +0530

    Update and rename Zzzzztest3 to githubhelp.py

---
+++ Yumeko/modules/githubhelp.py
@@ -0,0 +1,17 @@
+__mod_name__ = "Gɪᴛʜᴜʙ"
+
+__help__ = """
+*Github module. This module will fetch github releases*\n
+*Available commands:*
+ • /github <user> : will fetch the most recent GitHub user
+ • /git <user>/<repo>: will fetch the most recent release from that repo.
+ • /git <user>/<repo> <number>: will fetch releases in past.
+ • /fetch <reponame> or &reponame`: same as /git , but you can use a saved repo shortcut
+ • /listrepo: lists all repo shortcuts in chat
+ • /gitver: returns the current API version
+ • /changelog <reponame>: gets the changelog of a saved repo shortcut
+ 
+*Admin only:*
+ • /saverepo <name> <user>/<repo> <number (optional)>: saves a repo value as shortcut
+ • /delrepo <name>: deletes a repo shortcut
+"""
