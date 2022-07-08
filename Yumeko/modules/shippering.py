f0c4cf72bb354a8b9035946dd2c82f7579706cce
devroishiaka <108065240+devroishiaka@users.noreply.github.com>
2022-07-08 18:05:44 +0530

    Update shippering.py

--- Yumeko/modules/shippering.py
+++ Yumeko/modules/shippering.py
@@ -29,7 +29,9 @@ today = str(dt()[0])
 tomorrow = str(dt_tom())
 
 
-@app.on_message(filters.command("shipping"))
+
+@app.on_message(filters.command("couples"))
+
 @capture_err
 async def couple(_, message):
     if message.chat.type == "private":
@@ -40,7 +42,7 @@ async def couple(_, message):
         is_selected = await get_couple(chat_id, today)
         if not is_selected:
             list_of_users = []
-            async for i in app.iter_chat_members(message.chat.id):
+            async for i in app.get_chat_members(message.chat.id):
                 if not i.user.is_bot:
                     list_of_users.append(i.user.id)
             if len(list_of_users) < 2:
@@ -54,9 +56,8 @@ async def couple(_, message):
             c2_mention = (await app.get_users(c2_id)).mention
 
             couple_selection_message = f"""**Couple of the day:**
-
-{c1_mention} + {c2_mention} = hehe! good luck from hori san ❤️
-__New couple of the day may be chosen one eternity later{tomorrow}__"""
+{c1_mention} + {c2_mention} = ❤️
+__New couple of the day may be chosen at 12AM {tomorrow}__"""
             await app.send_message(message.chat.id, text=couple_selection_message)
             couple = {"c1_id": c1_id, "c2_id": c2_id}
             await save_couple(chat_id, today, couple)
@@ -66,8 +67,7 @@ __New couple of the day may be chosen one eternity later{tomorrow}__"""
             c2_id = int(is_selected["c2_id"])
             c1_name = (await app.get_users(c1_id)).first_name
             c2_name = (await app.get_users(c2_id)).first_name
-            couple_selection_message = f"""**Couple of the day hehe ///:**
-
+            couple_selection_message = f"""Couple of the day:
 [{c1_name}](tg://openmessage?user_id={c1_id}) + [{c2_name}](tg://openmessage?user_id={c2_id}) = ❤️
 __New couple of the day may be chosen at 12AM {tomorrow}__"""
             await app.send_message(message.chat.id, text=couple_selection_message)
@@ -77,3 +77,7 @@ __New couple of the day may be chosen at 12AM {tomorrow}__"""
 
 
 __mod_name__ = "Couples"
+__help__ = """
+*Couples / Shippering*
+ ❍ `/couples` :  get couples of the day!
+"""
