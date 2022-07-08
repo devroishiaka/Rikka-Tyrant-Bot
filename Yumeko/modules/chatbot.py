22e94f76b39299a0d331bab18ae318d2ad00e0cf
devroishiaka <108065240+devroishiaka@users.noreply.github.com>
2022-07-08 16:46:39 +0530

    Update chatbot.py

--- Yumeko/modules/chatbot.py
+++ Yumeko/modules/chatbot.py
@@ -125,7 +125,7 @@ def chatbot(update: Update, context: CallbackContext):
 
 def list_all_chats(update: Update, context: CallbackContext):
     chats = sql.get_all_kuki_chats()
-    text = "<b>ZeroTwo Enabled Chats</b>\n"
+    text = "<b>Chizuru Enabled Chats</b>\n"
     for chat in chats:
         try:
             x = context.bot.get_chat(int(*chat))
