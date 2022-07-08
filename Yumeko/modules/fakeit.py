d1573f2332493caec1394b1af4435ec59015c506
devroishiaka <108065240+devroishiaka@users.noreply.github.com>
2022-07-08 15:53:27 +0530

    Update fakeit.py

--- Yumeko/modules/fakeit.py
+++ Yumeko/modules/fakeit.py
@@ -44,7 +44,7 @@ async def _(event):
             with open("luna.jpg", "wb") as f:
                 f.write(response.content)
 
-        captin = f"Fake Image powered by @SiderzChat."
+        captin = f"Fake Image powered by @chizuruxdbot ."
         fole = "luna.jpg"
         await tbot.send_file(event.chat_id, fole, caption=captin)
         await event.delete()
