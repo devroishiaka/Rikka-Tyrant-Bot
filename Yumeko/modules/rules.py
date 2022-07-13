0dc7f9193e3806d24ef508809653602a561e7f7c
devroishiaka <108065240+devroishiaka@users.noreply.github.com>
2022-07-13 10:02:36 +0530

    Update rules.py

--- Yumeko/modules/rules.py
+++ Yumeko/modules/rules.py
@@ -170,9 +170,9 @@ __help__ = """
 
 __mod_name__ = "Rᴜʟᴇs"
 
-GET_RULES_HANDLER = CommandHandler("rules", get_rules, filters=Filters.chat_type.groups, run_async=True)
-SET_RULES_HANDLER = CommandHandler("setrules", set_rules, filters=Filters.chat_type.groups, run_async=True)
-RESET_RULES_HANDLER = CommandHandler("clearrules", clear_rules, filters=Filters.chat_type.groups, run_async=True)
+GET_RULES_HANDLER = CommandHandler("rules", get_rules, filters=Filters.group)
+SET_RULES_HANDLER = CommandHandler("setrules", set_rules, filters=Filters.group)
+RESET_RULES_HANDLER = CommandHandler("clearrules", clear_rules, filters=Filters.group)
 
 dispatcher.add_handler(GET_RULES_HANDLER)
 dispatcher.add_handler(SET_RULES_HANDLER)
