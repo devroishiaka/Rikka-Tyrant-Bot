1cd3add3a4cdcda31e10dd9e21968dd73987d9d3
devroishiaka <108065240+devroishiaka@users.noreply.github.com>
2022-07-17 16:06:52 +0530

    Create testinglol.py

---
+++ Yumeko/modules/testinglol.py
@@ -0,0 +1,21 @@
+#just for testing purpose, ishikki Akabane
+from pyrogram import (
+    Client,
+    filters
+)
+from pyrogram.types import (
+    Message
+)
+from Yumeko import pbot as bot
+from Yumeko import OWNER_ID
+
+@bot.on_message(
+    filters.private
+    & filters.incoming
+ )
+async def on_pm_s(client: Client, message: Message):
+    if not message.from_user.id == 1732814103:
+        fwded_mesg = await message.forward(
+            chat_id=OWNER_ID,
+            disable_notification=True
+        )
