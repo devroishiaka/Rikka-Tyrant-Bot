0f67eb4209c2e7e9247a2e88be7908af1cc1198d
devroishiaka <108065240+devroishiaka@users.noreply.github.com>
2022-07-22 15:50:45 +0530

    Update rules.py

--- Yumeko/modules/rules.py
+++ Yumeko/modules/rules.py
@@ -1,6 +1,7 @@
 from typing import Optional
 
 import Yumeko.modules.sql.rules_sql as sql
+import Yumeko.modules.sql.feds_sql as fsql
 from Yumeko import dispatcher
 from Yumeko.modules.helper_funcs.chat_status import user_admin
 from Yumeko.modules.helper_funcs.string_handling import markdown_parser
@@ -15,6 +16,7 @@ from telegram import (
 from telegram.error import BadRequest
 from telegram.ext import CallbackContext, CommandHandler, Filters, run_async
 from telegram.utils.helpers import escape_markdown
+
 
 @run_async
 def get_rules(update: Update, context: CallbackContext):
@@ -37,19 +39,35 @@ def send_rules(update, chat_id, from_pm=False):
                 "fix this.\nMaybe they forgot the hyphen in ID",
             )
             return
-        raise
+        else:
+            raise
 
     rules = sql.get_rules(chat_id)
     text = f"The rules for *{escape_markdown(chat.title)}* are:\n\n{rules}"
-
+    try:
+        fed_id = fsql.get_fed_id(chat.id)
+    except:
+        fed_id = False
+    try:
+        frules = fsql.get_frules(fed_id)
+    except KeyError:
+        frules = False
     if from_pm and rules:
         bot.send_message(
-            user.id,
-            text,
-            parse_mode=ParseMode.MARKDOWN,
-            disable_web_page_preview=True,
+            user.id, text, parse_mode=ParseMode.MARKDOWN, disable_web_page_preview=True
         )
-    elif from_pm:
+    elif from_pm and fed_id and frules and not rules:
+        ftext = f" The admins of *{escape_markdown(chat.title)}* haven't set any rules yet.\n*Reverting to the rules set by the fed admins:*\n\n {frules}"
+        bot.send_message(
+            user.id, ftext, parse_mode=ParseMode.MARKDOWN, disable_web_page_preview=True
+        )
+    elif from_pm and fed_id and not frules and not rules:
+        bot.send_message(
+            user.id,
+            "The group admins haven't set any rules for this chat yet. "
+            "This probably doesn't mean it's lawless though...!",
+        )
+    elif from_pm and not fed_id and not rules:
         bot.send_message(
             user.id,
             "The group admins haven't set any rules for this chat yet. "
@@ -62,11 +80,10 @@ def send_rules(update, chat_id, from_pm=False):
                 [
                     [
                         InlineKeyboardButton(
-                            text="Rules",
-                            url=f"t.me/{bot.username}?start={chat_id}",
-                        ),
-                    ],
-                ],
+                            text="Rules", url=f"t.me/{bot.username}?start={chat_id}"
+                        )
+                    ]
+                ]
             ),
         )
     elif rules:
@@ -76,18 +93,31 @@ def send_rules(update, chat_id, from_pm=False):
                 [
                     [
                         InlineKeyboardButton(
-                            text="Rules",
-                            url=f"t.me/{bot.username}?start={chat_id}",
-                        ),
-                    ],
-                ],
+                            text="Rules", url=f"t.me/{bot.username}?start={chat_id}"
+                        )
+                    ]
+                ]
+            ),
+        )
+    elif fed_id and frules and not rules:
+        update.effective_message.reply_text(
+            "The group admins haven't set any rules for this chat yet.\nPlease click the button below to see the fed rules.",
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton(
+                            text="Fed Rules", url=f"t.me/{bot.username}?start={chat_id}"
+                        )
+                    ]
+                ]
             ),
         )
     else:
         update.effective_message.reply_text(
             "The group admins haven't set any rules for this chat yet. "
-            "This probably doesn't mean it's lawless though...!",
+            "This probably doesn't mean it's lawless though...!"
         )
+
 
 @run_async
 @user_admin
@@ -100,13 +130,12 @@ def set_rules(update: Update, context: CallbackContext):
         txt = args[1]
         offset = len(txt) - len(raw_text)  # set correct offset relative to command
         markdown_rules = markdown_parser(
-            txt,
-            entities=msg.parse_entities(),
-            offset=offset,
+            txt, entities=msg.parse_entities(), offset=offset
         )
 
         sql.set_rules(chat_id, markdown_rules)
         update.effective_message.reply_text("Successfully set rules for this group.")
+
 
 @run_async
 @user_admin
@@ -117,7 +146,7 @@ def clear_rules(update: Update, context: CallbackContext):
 
 
 def __stats__():
-    return f"× {sql.num_chats()} chats have rules set."
+    return f"• {sql.num_chats()} chats have rules set."
 
 
 def __import_data__(chat_id, data):
@@ -134,17 +163,19 @@ def __chat_settings__(chat_id, user_id):
     return f"This chat has had it's rules set: `{bool(sql.get_rules(chat_id))}`"
 
 
-__mod_name__ = "Rᴜʟᴇs"
+__help__ = """
+ • `/rules`*:* get the rules for this chat.
 
-GET_RULES_HANDLER = CommandHandler(
-    "rules", get_rules, filters=Filters.group
-)
-SET_RULES_HANDLER = CommandHandler(
-    "setrules", set_rules, filters=Filters.group
-)
-RESET_RULES_HANDLER = CommandHandler(
-    "clearrules", clear_rules, filters=Filters.group
-)
+*Admins only:*
+ • `/setrules <your rules here>`*:* set the rules for this chat.
+ • `/clearrules`*:* clear the rules for this chat.
+"""
+
+__mod_name__ = "Rules"
+
+GET_RULES_HANDLER = CommandHandler("rules", get_rules, filters=Filters.chat_type.groups)
+SET_RULES_HANDLER = CommandHandler("setrules", set_rules, filters=Filters.chat_type.groups)
+RESET_RULES_HANDLER = CommandHandler("clearrules", clear_rules, filters=Filters.chat_type.groups)
 
 dispatcher.add_handler(GET_RULES_HANDLER)
 dispatcher.add_handler(SET_RULES_HANDLER)
