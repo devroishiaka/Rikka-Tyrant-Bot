d73d3703b9b3b8107df0b9888d4fe613a6416255
devroishiaka <108065240+devroishiaka@users.noreply.github.com>
2022-07-23 10:15:29 +0530

    Update reverse.py

--- Yumeko/modules/reverse.py
+++ Yumeko/modules/reverse.py
@@ -17,10 +17,10 @@ from Yumeko.modules.sql.clear_cmd_sql import get_clearcmd
 from Yumeko.modules.helper_funcs.misc import delete
 
 opener = urllib.request.build_opener()
-useragent = "Mozilla/5.0 (Linux; Android 6.0.1; SM-G920V Build/MMB29K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.98 Mobile Safari/537.36"
+useragent = "Mozilla/5.0 (Linux; Android 9; SM-G960F Build/PPR1.180610.011; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/74.0.3729.157 Mobile Safari/537.36"
 opener.addheaders = [("User-agent", useragent)]
 
-
+@run_async
 def reverse(update: Update, context: CallbackContext):
     if os.path.isfile("okgoogle.png"):
         os.remove("okgoogle.png")
@@ -220,7 +220,7 @@ __help__ = """*Image reverse:*
  """
 
 REVERSE_HANDLER = DisableAbleCommandHandler(
-    ["reverse","grs","pp","p"], reverse
+    ["reverse","grs","pp","p"], reverse, admin_ok=True
 )
 
 dispatcher.add_handler(REVERSE_HANDLER)
