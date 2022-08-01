66006a630fed8641bf6b9d1fc53c4bd68d0285a7
devroishiaka <108065240+devroishiaka@users.noreply.github.com>
2022-08-01 10:17:31 +0530

    Update progress.py

--- Yumeko/utils/progress.py
+++ Yumeko/utils/progress.py
@@ -38,15 +38,3 @@ async def progress(
             f"`Duration` -> {time_formatter(elapsed_time)}"
         )
         await gdrive.edit(f"`{prog_type}`\n\n" f"`Status`\n{tmp}")
-
-def get_text(message: Message) -> [None, str]:
-    text_to_return = message.text
-    if message.text is None:
-        return None
-    if " " in text_to_return:
-        try:
-            return message.text.split(None, 1)[1]
-        except IndexError:
-            return None
-    else:
-        return None
