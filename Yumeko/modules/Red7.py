5eda80b3eca4af414a938ce97c102634e1a3b767
Sugar <108065240+sugar-Ik@users.noreply.github.com>
2023-03-21 12:52:25 +0530

    Update and rename Red7.py.txt to Red7.py

--- Yumeko/modules/Red7.py
+++ Yumeko/modules/Red7.py
@@ -1,13 +1,15 @@
 from .. import pbot as Red7
 from pyrogram.types import Message
 from pyrogram import filters
-
+"""
 try:
   from RiZoeLX.functions import Red7_Watch
 except:
   import os
   os.system("pip3 install pyRiZoeLX")
   from RiZoeLX.functions import Red7_Watch
+"""
+from RiZoeLX.functions import Red7_Watch
 
 
 @Red7.on_message(filters.group & filters.all)
