b292900c416207b8ac113065ce587324a65b0941
Sugar <108065240+sugar-Ik@users.noreply.github.com>
2023-03-21 13:52:59 +0530

    Update db_migraton.py

--- Yumeko/modules/db_migraton.py
+++ Yumeko/modules/db_migraton.py
@@ -9,19 +9,15 @@ from Yumeko import (
 )
 
 
-# Define the backup command
+@run_async
 def db_backup(update: Update, context: CallbackContext):
-    # Get the database credentials
-    db_host = "dpg-cfp1k7h4rebfdaqpper0-a"
-    db_port = 5432
-    db_name = "chizuru_m4xr"
-    db_user = "chizuru"
-    db_password = "PbOsQzJfjaSfDizJv67ZGjCf1hfWHanU"
+    user_id = update.effective_user.id
+    if user_id != OWNER_ID:
+        update.message.reply_text("Only for owner!!")
+        return
+    
     backup_dir = "./"
-    """
-    backup_file = f"{backup_dir}/{db_name}.backup"
-    backup_command = f"pg_dump -h {db_host} -p {db_port} -U {db_user} -F c -b -v {db_name} > {backup_file}"
-    """
+    backup_file = f"{backup_dir}/backup.sql"
     
     backup_command = "pg_dump 'postgres://chizuru:PbOsQzJfjaSfDizJv67ZGjCf1hfWHanU@dpg-cfp1k7h4rebfdaqpper0-a.oregon-postgres.render.com/chizuru_m4xr' > backup.sql"
     # Execute the backup command
@@ -30,10 +26,29 @@ def db_backup(update: Update, context: CallbackContext):
     # Send a message to the user with the backup file
     try:
         with open(backup_file, "rb") as file:
-            update.message.reply_document(file, filename=f"{db_name}.backup")
+            update.message.reply_document(file, filename="backup.sql")
     except Exception as e:
         update.message.reply_text(e)
 
+        
+@run_async
+def db_migrate(update: Update, context: CallbackContext):
+    user_id = update.effective_user.id
+    if user_id != OWNER_ID:
+        update.message.reply_text("Only for owner!!")
+        return
+    
+    backup_dir = "./"
+    backup_file = f"{backup_dir}/backup.sql"
+    
+    try:
+        backup_command = "psql 'postgres://chizuru:e16mjneLHdty2uBriNd9X0rUVu8r7VHD@dpg-cgbdnbpmbg55nqlq0c70-a.oregon-postgres.render.com/chizuru_qiea' -f backup.sql"
+        # Execute the backup command
+        subprocess.run(backup_command, shell=True, check=True)
+        update.message.reply_text("Migration started!")
+    except Exception as e:
+        update.message.reply_text(e)
 
+        
 dispatcher.add_handler(CommandHandler("db_export", db_backup))
-#dispatcher.add_handler(CommandHandler("db_import", db_migrate))
+dispatcher.add_handler(CommandHandler("db_import", db_migrate))
