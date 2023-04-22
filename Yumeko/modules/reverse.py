853b85e54eba0e62a0553f11239a937494fee64d
Sugar <108065240+sugar-Ik@users.noreply.github.com>
2023-04-22 17:52:33 +0530

    Update reverse.py

--- Yumeko/modules/reverse.py
+++ Yumeko/modules/reverse.py
@@ -1,60 +1,47 @@
 import os
-import json
-import requests
 import aiohttp
-import json
 
-from telegram.ext import CommandHandler
+from pyrogram import Client, filters
+from pyrogram.types import Message
 
-from Yumeko import dispatcher
+from Yumeko import pbot
 
 api_key = "blue-api-testing"
 url = 'https://blue-api.vercel.app/reverse'
 
-
-async def reverse(update, context):
-    message = update.effective_message
-    chat_id = update.effective_chat.id
+@pbot.on_message(filters.command(["pp", "grs", "p", "reverse"]))
+async def reverse(client, message):
+    chat_id = message.chat.id
     rtmid = message.message_id
 
     reply = message.reply_to_message
-
-    if reply:
-        if reply.sticker:
-            file_id = reply.sticker.file_id
-            new_id = reply.sticker.file_unique_id
-        elif reply.photo:
-            file_id = reply.photo[-1].file_id
-            new_id = reply.photo[-1].file_unique_id
-        else:
-            await message.reply_text("Reply To An Image Or Sticker To Lookup!")
-            return
-
-        file_path = os.path.join("temp", f"{new_id}.jpg")
-        file_obj = await context.bot.get_file(file_id)
-        file_url = file_obj.file_path
-
-    else:
-        await message.reply_text(
-            "Please Reply To A Sticker, Or An Image To Search It!"
-        )
+    if not reply:
+        await message.reply_text("Please reply to a sticker or an image to search it!")
         return
+    
+
+    if reply.sticker:
+        file_id = reply.sticker.file_id
+        new_id = reply.sticker.file_unique_id
+    elif reply.photo:
+        file_id = reply.photo[-1].file_id
+        new_id = reply.photo[-1].file_unique_id
+    else:
+        await message.reply_text("Reply to an image or sticker to lookup!")
+        return
+
+    file_path = os.path.join("temp", f"{new_id}.jpg")
+    file_obj = await client.download_media(message=reply)
+    file_url = file_obj.file_path
 
     try:
         data = {"img_url": file_url}
         headers = {"API-KEY": api_key}
 
-        try:
-            async with aiohttp.ClientSession() as session:
-                async with session.post(url, headers=headers, json=data) as resp:
-                    response_text = await resp.text()
+        async with aiohttp.ClientSession() as session:
+            async with session.post(URL, headers=headers, json=data) as resp:
+                response_text = await resp.text()
 
-            await message.reply_text(response_text)
-        except:
-            await message.reply_text("Cant find anything!!")
-            
+        await message.reply_text(response_text)
     except Exception as e:
-        pass
-
-
-dispatcher.add_handler(CommandHandler(["pp", "grs", "p", "reverse"], reverse))
+        await message.reply_text("Can't find anything!")
