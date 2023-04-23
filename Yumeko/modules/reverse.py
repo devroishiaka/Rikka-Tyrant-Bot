5c5dd61f084f1557eeb6d146dad30a1168dc9947
Sugar <108065240+sugar-Ik@users.noreply.github.com>
2023-04-23 14:59:06 +0530

    Update reverse.py

--- Yumeko/modules/reverse.py
+++ Yumeko/modules/reverse.py
@@ -16,12 +16,11 @@ async def reverse(client, message):
     if message.reply_to_message and (
         message.reply_to_message.photo or message.reply_to_message.sticker
     ):
-        file_id = message.reply_to_message.photo[-1].file_id
-        new_id = message.reply_to_message.photo[-1].file_unique_id
+        file_id = message.reply_to_message.photo.file_id
+        new_id = message.reply_to_message.photo.file_unique_id
         file_path = os.path.join("temp", f"{new_id}.jpg")
-        file_info = await app.get_messages(message.reply_to_message.chat.id, message.reply_to_message.id)
-        file_location = file_info.photo.file_location
-        file_url = f"https://api.telegram.org/file/bot{app.api_id}:{app.api_hash}/{file_location}"
+        file_obj = await client.get_file(file_id)
+        file_url = file_obj.file_path
         print(file_url)
 
         try:
